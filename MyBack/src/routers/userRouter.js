f020d175004d4946dfb8e6679f83a91a0cd4db97
DongHyun Kim <developerforus@gmail.comgit config --global user.email developerforus@gmail.com>
2022-03-16 23:09:02 +0900

    user_id req.params로 받기

--- MyBack/src/routers/userRouter.js
+++ MyBack/src/routers/userRouter.js
@@ -65,6 +65,16 @@ userAuthRouter.get("/user/current", verifyToken, async (req, res, next) => {
 
 userAuthRouter.get("/users/:id", verifyToken, async (req, res, next) => {
   try {
+    const user_id = req.params.id;
+    console.log(user_id);
+    const currentUser = await userAuthService.getUserInfo({ user_id });
+    console.log(currentUser);
+
+    if (currentUser.errorMessage) {
+      throw new Error(currentUser.errorMessage);
+    }
+
+    res.status(200).json(currentUser);
   } catch (error) {
     next(error);
   }
