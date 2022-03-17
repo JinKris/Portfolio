0e8e15a91a43374814cbf96d18203269900397c0
DongHyun Kim <developerforus@gmail.comgit config --global user.email developerforus@gmail.com>
2022-03-17 10:45:36 +0900

    award

--- award/MyBack/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("./src/app");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-PORT = process.env.SERVER_PORT || 5000;
-
-app.listen(PORT, console.log(`${PORT}번 포트 오온!`));
