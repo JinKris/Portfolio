ca9a4af341f547b9bef2d74004173e07ea89831b
JinKris <vain65@naver.com>
2022-03-17 10:45:14 +0900

    fix error

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -5,7 +5,7 @@ import { Container, Col, Row } from "react-bootstrap";
 import { UserStateContext } from "../App";
 import * as Api from "../api";
 import User from "./user/User";
-import Certificate from "./certificate/Certificate"
+import Certificates from "./certificate/Certificates"
 
 function Portfolio() {
   const navigate = useNavigate();
@@ -63,7 +63,7 @@ function Portfolio() {
         </Col>
         <Col>
 
-        <Certificate
+        <Certificates
             portfolioOwnerId={portfolioOwner.id}
             isEditable={portfolioOwner.id === userState.user?.id}
           />
