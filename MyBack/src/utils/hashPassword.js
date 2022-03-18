1b99e9d6514a032ef8db0ba49bc78829751c9548
DongHyun Kim <developerforus@gmail.comgit config --global user.email developerforus@gmail.com>
2022-03-18 17:14:36 +0900

    MyBack삭제

--- MyBack/src/utils/hashPassword.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcrypt");
-
-const hashPassword = async (password, num) => {
-  const hashedPassword = await bcrypt.hash(password, num);
-  return hashedPassword;
-};
-
-module.exports = hashPassword;
